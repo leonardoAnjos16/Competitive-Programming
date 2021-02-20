f13850d4474fa6f53b1701cd033ef58cde9ed0b9
Leonardo Anjos <las4@cin.ufpe.br>
2021-02-20 19:50:13 -0300

    Added template file

---
+++ base.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define long long long int
+
+#define pii pair<int, int>
+#define pll pair<long, long>
+#define fst first
+#define snd second
+
+#define heap(ds) priority_queue<ds>
+#define heap(ds, cmp) priority_queue<ds, vector<ds>, cmp>
+
+const int MAX = 2e5 + 5;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
