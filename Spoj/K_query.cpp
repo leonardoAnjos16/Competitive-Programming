3b35df45bede81a8a9ed7c158dc60bb17c16c3c5
Leonardo Anjos <las4@cin.ufpe.br>
2022-01-09 20:32:42 -0300

    Resolved a Spoj problem using persistent segment tree

--- Spoj/K_query.cpp
+++ Spoj/K_query.cpp
@@ -6,48 +6,72 @@ using namespace std;
 
 struct SegmentTree {
 private:
+    struct Node {
+        int zeroes;
+        Node *left, *right;
+
+        Node(int zeroes): zeroes(zeroes), left(nullptr), right(nullptr) {}
+        Node(Node *left, Node *right): left(left), right(right) {
+            zeroes = 0;
+            if (left) zeroes += left->zeroes;
+            if (right) zeroes += right->zeroes;
+        }
+    };
+
     int n;
     vector<int> a;
-    vector<vector<int>> tree;
+    vector<Node*> roots;
 
 public:
     SegmentTree(int n, vector<int> &a) {
         this->n = n;
-        this->a = a;
+        roots.push_back(build(1, n));
 
-        tree.assign(4 * n, vector<int>());
-        build(1, 1, n);
+        map<int, vector<int>> pos;
+        for (int i = 0; i < n; i++)
+            pos[a[i]].push_back(i + 1);
+
+        for (auto [value, positions]: pos)
+            for (auto p: positions)
+                update(p);
+
+        sort(a.begin(), a.end());
+        this->a = a;
+    }
+
+    void update(int i) {
+        roots.push_back(update(roots.back(), 1, n, i));
     }
 
     int query(int l, int r, int k) {
-        return query(1, 1, n, l, r, k);
+        int idx = upper_bound(a.begin(), a.end(), k) - a.begin();
+        return query(roots[idx], 1, n, l, r);
     }
 
 private:
-    void merge_trees(int i) {
-        merge(tree[2 * i].begin(), tree[2 * i].end(), tree[2 * i + 1].begin(), tree[2 * i + 1].end(), back_inserter(tree[i]));
+    Node *build(int l, int r) {
+        if (l == r) return new Node(1);
+
+        int mid = (l + r) / 2;
+        Node *left = build(l, mid);
+        Node *right = build(mid + 1, r);
+        return new Node(left, right);
     }
 
-    int smaller(int i, int k) {
-        return upper_bound(tree[i].begin(), tree[i].end(), k) - tree[i].begin();
+    Node *update(Node *node, int l, int r, int i) {
+        if (l == r) return new Node(node->zeroes - 1);
+
+        int mid = (l + r) / 2;
+        if (i <= mid) return new Node(update(node->left, l, mid, i), node->right);
+        return new Node(node->left, update(node->right, mid + 1, r, i));
     }
 
-    void build(int i, int l, int r) {
-        if (l == r) tree[i] = vector<int>(1, a[l]);
-        else {
-            int m = (l + r) / 2;
-            build(2 * i, l, m);
-            build(2 * i + 1, m + 1, r);
-            merge_trees(i);
-        }
-    }
-
-    int query(int i, int l, int r, int ql, int qr, int k) {
+    int query(Node *node, int l, int r, int ql, int qr) {
         if (r < ql || l > qr) return 0;
-        if (ql <= l && r <= qr) return r - l - smaller(i, k) + 1;
+        if (ql <= l && r <= qr) return node->zeroes;
 
-        int m = (l + r) / 2;
-        return query(2 * i, l, m, ql, qr, k) + query(2 * i + 1, m + 1, r, ql, qr, k);
+        int mid = (l + r) / 2;
+        return query(node->left, l, mid, ql, qr) + query(node->right, mid + 1, r, ql, qr);
     }
 };
 
@@ -57,8 +81,8 @@ int main() {
 
     int n; cin >> n;
 
-    vector<int> a(n + 1);
-    for (int i = 1; i <= n; i++)
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
         cin >> a[i];
 
     int q; cin >> q;
