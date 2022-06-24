703bc06ebc46931a92e7d37fde546099e073ff7a
Leonardo Anjos <leonardoanjos.1a2015@gmail.com>
2022-06-23 23:06:19 -0300

    Updated max flow problem from Codeforces to use Dinic's algorithm

--- Codeforces/array_and_operations.cpp
+++ Codeforces/array_and_operations.cpp
@@ -4,107 +4,167 @@ using namespace std;
 
 #define long long long int
 
-int n;
-vector<vector<int>> g, c;
+template<typename T = int>
+struct MaxFlow {
+private:
+    struct Edge {
+        int from, to;
+        T cap, flow;
 
-int find_path(int s, int t, vector<int> &p) {
-    p.assign(n + 2, -1);
-    p[s] = s;
+        Edge(int from, int to, T cap) {
+            this->from = from;
+            this->to = to;
+            this->cap = cap;
+        }
+    };
 
-    queue<pair<int, int>> aux;
-    aux.push(make_pair(s, INT_MAX));
+    int n, src, snk;
+    vector<Edge> edges;
+    vector<vector<int>> ids;
+    vector<int> depth, first;
 
-    while (!aux.empty()) {
-        int v = aux.front().first;
-        int flow = aux.front().second;
-        aux.pop();
+public:
+    MaxFlow(int n) {
+        this->n = n;
+        ids.assign(n, vector<int>());
+    }
 
-        for (auto u: g[v]) {
-            if (p[u] == -1 && c[v][u]) {
-                p[u] = v;
-                flow = min(flow, c[v][u]);
+    void add_edge(int u, int v, T cap) {
+        int m = edges.size();
 
-                if (u == t) return flow;
-                aux.push(make_pair(u, flow));
+        edges.push_back(Edge(u, v, cap));
+        ids[u].push_back(m);
+
+        edges.push_back(Edge(v, u, 0));
+        ids[v].push_back(m + 1);
+    }
+
+    T max_flow(int src, int snk) {
+        const T INF = numeric_limits<T>::max();
+
+        this->src = src;
+        this->snk = snk;
+
+        for (auto &e: edges)
+            e.flow = 0;
+
+        T ans = 0;
+        while (get_depths()) {
+            T flow;
+            first.assign(n, 0);
+
+            while ((flow = find_path(src, INF)) > 0)
+                ans += flow;
+        }
+
+        return ans;
+    }
+
+private:
+    bool get_depths() {
+        depth.assign(n, -1);
+        depth[src] = 0;
+
+        queue<int> aux;
+        aux.push(src);
+
+        while (!aux.empty()) {
+            int v = aux.front(); aux.pop();
+            for (auto id: ids[v]) {
+                auto [from, u, cap, flow] = edges[id];
+                if (cap - flow > 0 && depth[u] == -1) {
+                    aux.push(u);
+                    depth[u] = depth[v] + 1;
+                }
             }
         }
+
+        return depth[snk] != -1;
     }
 
-    return 0;
-}
+    T find_path(int v, T flow) {
+        if (flow == 0) return 0;
+        if (v == snk) return flow;
 
-int max_flow(int s, int t) {
-    vector<int> p;
-    int ans = 0, flow;
+        while (first[v] < (int) ids[v].size()) {
+            int id = ids[v][first[v]++];
+            Edge e = edges[id];
 
-    while (flow = find_path(s, t, p)) {
-        ans += flow;
-
-        int curr = t;
-        while (curr != s) {
-            c[p[curr]][curr] -= flow;
-            c[curr][p[curr]] += flow;
-            curr = p[curr];
+            if (depth[e.to] == depth[v] + 1) {
+                T curr = find_path(e.to, min(flow, e.cap - e.flow));
+                if (curr > 0) {
+                    edges[id].flow += curr;
+                    edges[id ^ 1].flow -= curr;
+                    return curr;
+                }
+            }
         }
+
+        return 0;
+    }
+};
+
+int factors(int v, int p) {
+    int cnt = 0;
+    while (!(v % p)) {
+        cnt++;
+        v /= p;
     }
 
-    return ans;
+    return cnt;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int m;
+    int n, m;
     cin >> n >> m;
 
-    vector<map<int, int>> a(n + 1);
-    for (int i = 1; i <= n; i++) {
-        int ai; cin >> ai;
-        for (long j = 2LL; j * j <= ai; j++)
-            while (!(ai % j)) a[i][j]++, ai /= j;
+    vector<int> a(n);
+    vector<int> primes;
 
-        if (ai > 1) a[i][ai]++;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+
+        int aux = a[i];
+        for (int j = 2; j * j <= aux; j++) {
+            if (!(aux % j)) primes.push_back(j);
+            while (!(aux % j)) aux /= j;
+        }
+
+        if (aux > 1) primes.push_back(aux);
     }
 
-    g.assign(n + 2, vector<int>());
-    for (int i = 2; i <= n; i += 2) {
-        g[0].push_back(i);
-        g[i].push_back(0);
+    sort(primes.begin(), primes.end());
+    primes.erase(unique(primes.begin(), primes.end()), primes.end());
+
+    vector<pair<int, int>> pairs(m);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+
+        if (u & 1) swap(u, v);
+        pairs[i] = make_pair(u, v);
     }
-
-    for (int i = 1; i <= n; i += 2) {
-        g[i].push_back(n + 1);
-        g[n + 1].push_back(i);
-    }
-
-    while (m--) {
-        int i, j;
-        cin >> i >> j;
-
-        g[i].push_back(j);
-        g[j].push_back(i);
-    }
-
-    set<int> primes;
-    for (int i = 1; i <= n; i++)
-        for (auto p: a[i])
-            primes.insert(p.first);
 
     int ans = 0;
-    for (auto p: primes) {
-        c.assign(n + 2, vector<int>(n + 2, 0));
-        for (int i = 2; i <= n; i += 2)
-            c[0][i] = a[i][p];
+    for (int prime: primes) {
+        MaxFlow solver(n + 2);
+        int src = 0, snk = n + 1;
 
-        for (int i = 1; i <= n; i += 2)
-            c[i][n + 1] = a[i][p];
+        for (int i = 1; i <= n; i++) {
+            int cnt = factors(a[i - 1], prime);
+            if (i & 1) solver.add_edge(i, snk, cnt);
+            else solver.add_edge(src, i, cnt);
+        }
 
-        for (int i = 2; i <= n; i += 2)
-            for (auto j: g[i])
-                c[i][j] = min(a[i][p], a[j][p]);
+        for (auto [i, j]: pairs) {
+            int cnt = min(factors(a[i - 1], prime), factors(a[j - 1], prime));
+            solver.add_edge(i, j, cnt);
+        }
 
-        ans += max_flow(0, n + 1);
+        ans += solver.max_flow(src, snk);
     }
 
     cout << ans << "\n";
