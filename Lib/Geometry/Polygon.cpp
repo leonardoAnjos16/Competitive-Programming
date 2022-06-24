1ec3f89e3bd37052d10048ba06c709f6092bb724
Leonardo Anjos <leonardoanjos.1a2015@gmail.com>
2022-06-24 20:00:40 -0300

    Added convex hull

--- Lib/Geometry/Polygon.cpp
+++ Lib/Geometry/Polygon.cpp
@@ -7,3 +7,31 @@ bool inside(vector<Point<T>> &vertices, Point<T> P) {
 
     return true;
 }
+
+template<typename T = long>
+vector<Point<T>> half_hull(vector<Point<T>> &points, int l, int r, int d) {
+    vector<Point<T>> hull;
+    for (int i = l; i != r; i += d) {
+        int size = hull.size();
+        while (size > 1 && !right_turn(hull[size - 1] - hull[size - 2], points[i] - hull[size - 2]))
+            hull.pop_back(), size--;
+
+        hull.push_back(points[i]);
+    }
+
+    return hull;
+}
+
+template<typename T = long>
+vector<Point<T>> convex_hull(vector<Point<T>> &points) {
+    int n = points.size();
+    sort(points.begin(), points.end());
+
+    vector<Point<T>> hull = half_hull(points, 0, n, 1);
+    vector<Point<T>> lower = half_hull(points, n - 1, -1, -1);
+
+    for (int i = 1; i < (int) lower.size() - 1; i++)
+        hull.push_back(lower[i]);
+
+    return hull;
+}
