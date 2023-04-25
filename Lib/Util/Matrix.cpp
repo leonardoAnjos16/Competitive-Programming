bcd6ec6ad0c4fea2b10a05ad974fab52280f18d8
Leonardo Anjos <leonardoanjos.1a2015@gmail.com>
2023-04-25 15:44:25 -0300

    Updated Matrix

--- Lib/Util/Matrix.cpp
+++ Lib/Util/Matrix.cpp
@@ -1,4 +1,4 @@
-template<typename T = llong>
+template<typename T>
 struct Matrix {
     int rows, cols;
     T m[MAX][MAX];
@@ -7,19 +7,13 @@ struct Matrix {
         memset(m, 0, sizeof m);
     }
 
-    Matrix(T m[MAX][MAX], int rows, int cols): rows(rows), cols(cols) {
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < cols; j++)
-                this->m[i][j] = m[i][j];
-    }
-
     Matrix operator *(Matrix &other) {
         Matrix ans(rows, other.cols);
         for (int i = 0; i < rows; i++)
             for (int j = 0; j < other.cols; j++)
                 for (int k = 0; k < cols; k++) {
-                    ans.m[i][j] += m[i][k] * other.m[k][j];
-                    ans.m[i][j] %= MOD;
+                    ans[i][j] += m[i][k] * other[k][j];
+                    ans[i][j] %= MOD;
                 }
 
         return ans;
