35c5bf36750d34bd241bd0c44a6774acef87d91d
Leonardo Anjos <leonardoanjos.1a2015@gmail.com>
2023-07-22 03:18:05 -0300

    Added function for finding the point in the convex hull which maximizes scalar product with a given vector

--- Lib/Geometry/Polygon.cpp
+++ Lib/Geometry/Polygon.cpp
@@ -36,6 +36,42 @@ vector<Point<T>> convex_hull(vector<Point<T>> &points) {
     return hull;
 }
 
+template<typename T>
+int max_dot_product(vector<Point<T>> &hull, Vector<T> v) {
+    int n = hull.size();
+    if (n < 3) {
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+            if (hull[i] * v > hull[ans] * v)
+                ans = i;
+
+        return ans;
+    }
+
+    int ans = 0;
+    if (hull[1] * v > hull[ans] * v)
+        ans = 1;
+
+    for (int i = 0; i < 2; i++) {
+        int l = 2, r = n - 1;
+        while (l != r) {
+            int mid = (l + r + 1) / 2;
+
+            bool check = hull[mid] * v >= hull[mid - 1] * v;
+            if (!i) check &= hull[mid] * v >= hull[0] * v;
+            else check |= hull[mid - 1] * v < hull[0] * v;
+
+            if (check) l = mid;
+            else r = mid - 1;
+        }
+
+        if (hull[l] * v > hull[ans] * v)
+            ans = l;
+    }
+
+    return ans;
+}
+
 template<typename T = llong>
 T area(Point<T> A, Point<T> B, Point<T> C) {
     return (B - A) / (C - B);
