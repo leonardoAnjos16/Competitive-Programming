16b073d924867b104ba430cdb7767b549999c67a
Leonardo Anjos <leonardoanjos.1a2015@gmail.com>
2023-08-01 19:41:16 -0300

    Added incircle and some other auxiliary functions to Point

--- Lib/Geometry/Point.cpp
+++ Lib/Geometry/Point.cpp
@@ -81,6 +81,42 @@ Point<T> proj_line(Point<T> P, Point<T> A, Point<T> B) {
 }
 
 template<typename T>
+Vector<ldouble> rotate(Vector<T> v, ldouble theta) {
+    return Vector<ldouble>(v.x * cosl(theta) - v.y * sinl(theta), v.x * sinl(theta) + v.y * cosl(theta));
+}
+
+template<typename T>
+Vector<ldouble> bisect(Vector<T> u, Vector<T> v) {
+    ldouble theta = angle(u, v);
+    return left_turn(u, v) ? rotate(u, theta / 2.0L) : rotate(v, theta / 2.0L);
+}
+
+template<typename T>
+pair<Point<ldouble>, ldouble> incircle(Point<T> A, Point<T> B, Point<T> C) {
+    Vector<T> AB = B - A, BC = C - B, CA = A - C;
+    ldouble a = abs(AB), b = abs(BC), c = abs(CA), s = (a + b + c) / 2.0L;
+    ldouble r = sqrtl(s - a) * sqrtl(s - b) * sqrtl(s - c) / sqrtl(s);
+
+    Vector<ldouble> u = bisect(AB, C - A);
+    Line<ldouble> l(A, u);
+
+    Vector<ldouble> v = bisect(BC, A - B);
+    Line<ldouble> m(B, v);
+
+    auto [P, has_intersection] = l.intersection(m);
+    assert(has_intersection);
+    return make_pair(P, r);
+}
+
+// Checks if P lies inside segment AB (assuming it lies inside line AB)
+template<typename T>
+bool between(Point<T> P, Point<T> A, Point<T> B) {
+    auto [lx, rx] = minmax(A.x, B.x);
+    auto [ly, ry] = minmax(A.y, B.y);
+    return sign(lx - P.x) <= 0 && sign(P.x - rx) <= 0 && sign(ly - P.y) <= 0 && sign(P.y - ry) <= 0;
+}
+
+template<typename T>
 bool k_lines(vector<Point<T>> points, int k) {
     if (!k) return points.empty();
     if ((int) points.size() <= k) return true;
